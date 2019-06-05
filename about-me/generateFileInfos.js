ef16882472a65a5a45e16fb00469696e04a29076
DashChen <zacxd7405@gmail.com>
2019-06-05 17:40:31 +0800

    取得md檔案內容長度 與 指定節點內文字，還有標籤要取回

--- about-me/generateFileInfos.js
+++ about-me/generateFileInfos.js
@@ -1,38 +1,63 @@
 const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
-const getFilesList = (srcPath, files = [], getDirectory = false) => {
-  const files_ = files || []
-  fs.readdirSync(srcPath).filter(file => {
-    if (fs.statSync(path.join(srcPath, file)).isDirectory()) {
-      if (getDirectory) {
-        files_.push(file)
-      }
-      getFilesList(path.join(srcPath, file), files_, getDirectory)
-    } else if (!getDirectory) {
-      let stat = fs.statSync(path.join(srcPath, file))
-      files_.push({
-        name: file,
-        size: stat.size,
-        ctime: Date.parse(stat.ctime),
-        mtime: Date.parse(stat.mtime)
-      })
-      if (path.extname(file).includes('.md')) {
-        // readline
-        //   .createInterface({
-        //     input: fs.createReadStream(path.join(srcPath, file))
-        //   })
-        //   .on('line', function(line) {
-        //     console.log('Line from file:', line)
-        //   })
-        let text = fs.readFileSync(path.join(srcPath, file), 'utf8')
-        // 取文字總數 regex /<\s*[^>]*>|<\s*/\s*>/g
-        // 取出指定i標籤 regex /<\s*(i)[^>]*>(.*?)</\s*[^>]*(i)>/g
-        console.log(text)
-      }
+
+const getMdInfo = (srcPath, file) => {
+    let stat = fs.statSync(path.join(srcPath, file))
+    if (stat) {
+        let info = {
+            name: file,
+            size: stat.size,
+            ctime: Date.parse(stat.ctime),
+            mtime: Date.parse(stat.mtime)
+        }
+        if (path.extname(file).includes('.md')) {
+            // readline
+            //   .createInterface({
+            //     input: fs.createReadStream(path.join(srcPath, file))
+            //   })
+            //   .on('line', function(line) {
+            //     console.log('Line from file:', line)
+            //   })
+            let text = fs.readFileSync(path.join(srcPath, file), 'utf8')
+            // 取文字總數 regex /<\s*[^>]*>|<\s*\/\s*>/g
+            // 取出指定i標籤 regex /<\s*(i)[^>]*>(.*?)<\/\s*[^>]*(i)>/g
+            // 取出文字長度(先去空白) regex /<s*[^>]*>|<\/s*[^>]*>|[#*`>\-\~\!\[\]]/g
+
+            // 去空白
+            text = text.replace(/\s/g, '');
+            // 取出字數
+            info["wordLength"] = text.replace(/<s*[^>]*>|<\/s*[^>]*>|[#*`>\-\~\!\[\]]/g, '').length
+            // 取出 summary
+            if (text.match(/<\s*(summary)[^>]*>(.*?)<\/\s*(summary)>/g)) {
+                info["summary"] = (text.match(/<\s*(summary)[^>]*>(.*?)<\/\s*(summary)>/g)[0]).replace(/<\s*[^>]*>|<\s*\/\s*>/g, '')
+            }
+        }
+        return info
     }
-  })
-  return files_
+    return null
+
 }
-console.log(getFilesList('./pages/', []))
-console.log(getFilesList('./pages/', [], true))
+
+const getFilesList = (srcPath, files = [], callback = null, getDirectory = false) => {
+    const files_ = files || []
+    fs.readdirSync(srcPath).filter(file => {
+        if (fs.statSync(path.join(srcPath, file)).isDirectory()) {
+            if (getDirectory) {
+                files_.push(file)
+            }
+            getFilesList(path.join(srcPath, file), files_, getDirectory)
+        } else if (!getDirectory) {
+            if (typeof callback == "function") {
+                files_.push(callback(srcPath, file))
+            } else {
+                files_.push(file)
+            }
+        }
+    })
+    return files_
+}
+console.log(getFilesList('./pages/', [], getMdInfo))
+
+
+// console.log(getFilesList('./pages/', [], true))
